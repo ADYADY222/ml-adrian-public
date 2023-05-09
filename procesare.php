4e4d4144d8ab3b5236481a711e88ef9450e6256b
ADYADY222 <60630331+ADYADY222@users.noreply.github.com>
2023-05-09 12:06:00 +0100

    Update

--- procesare.php
+++ procesare.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 ?>
 	<div class="button2">
-        <a href="http://localhost/ml-adrian-public/register.php"><button>Volver a registrarse</button></a>
+        <a href="http://localhost/ml-adrian-public/register.html"><button>Volver a registrarse</button></a>
     </div>
 </body>
 </html>
