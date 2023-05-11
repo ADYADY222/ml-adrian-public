1b8d3a3a5f369d350803ccce049d11f66ae2ed83
ADYADY222 <60630331+ADYADY222@users.noreply.github.com>
2023-05-11 11:36:23 +0100

    Create locations_default.php

---
+++ locations_default.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro-v6@44659d9/css/all.min.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <a href="index.html" style="text-decoration: none;"><h1>Descubriendo Gran Canaria</h1></a>
+  <div style="display: flex; justify-content: center;">
+    <a href="register.html"><button>Nuevo Lugar que visitar</button></a>
+    <a href="locations.html"><button>Lugares que no me puedo perder</button></a>
+  </div>
+  </head>
+  <body>
+    <div class="container2">
+    <h1 class="text3">Lugares que no me puedo perder</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Visitado</th>
+          <th>Nombre del lugar</th>
+          <th>Description</th>
+          <th>Municipio</th>
+          <th>Url con mas informacion</th>
+          <th>URL de Google Maps</th>
+          <th>Imagen</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <form action="/action_page.php">
+              <input type="checkbox" id="Agaete" name="Agaete" value="Agaete">
+            </form>
+          </td>
+          <td>1</td>
+          <td>Playa de Las Canteras</td>
+          <td>Las Palmas de Gran Canaria</td>
+          <td><a href="https://miplayadelascanteras.com">Página Oficial de Agaete</a></td>
+          <td><a href="https://shorturl.at/jPUY7"><i class="fa-duotone fa-map-location-dot fa-bounce" style=" --fa-bounce-start-scale-x: 1; --fa-bounce-start-scale-y: 1; --fa-bounce-jump-scale-x: 1; --fa-bounce-jump-scale-y: 1; --fa-bounce-land-scale-x: 1; --fa-bounce-land-scale-y: 1; " ></i></a></td>
+          <td><img class="pozica" src="https://shorturl.at/msAF9" alt="Agaete"></td>
+          </form>
+        </tr>
+        <tr>
+          <td>
+            <form action="/action_page.php">
+              <input type="checkbox" id="Agaete" name="Agaete" value="Agaete">
+            </form>
+          </td>
+          <td>2</td>
+          <td>Agüimes es una localidad y municipio español perteneciente a la isla de Gran Canaria</td>
+          <td>Aguimes</td>
+          <td><a href="https://aguimes.es/">Página Oficial de Agaete</a></td>
+          <td><a href="https://shorturl.at/oyHT8"><i class="fa-duotone fa-map-location-dot fa-bounce" style=" --fa-bounce-start-scale-x: 1; --fa-bounce-start-scale-y: 1; --fa-bounce-jump-scale-x: 1; --fa-bounce-jump-scale-y: 1; --fa-bounce-land-scale-x: 1; --fa-bounce-land-scale-y: 1; " ></i></a></td>
+          <td><img class="pozica" src="https://shorturl.at/luvLS" alt="Aguimes"></td>
+        </tr>
+        <tr>
+          <td>
+            <form action="/action_page.php">
+              <input type="checkbox" id="Artenara" name="Artenara" value="Artenara">
+            </form>
+          </td>
+          <td>3</td>
+          <td>Artenara es una localidad y municipio español perteneciente a la isla de Gran Canaria</td>
+          <td>Artenara</td>
+          <td><a href="https://www.artenara.es/">Página Oficial de Agaete</a></td>
+          <td><a href="https://shorturl.at/oqvCR"><i class="fa-duotone fa-map-location-dot fa-bounce" style=" --fa-bounce-start-scale-x: 1; --fa-bounce-start-scale-y: 1; --fa-bounce-jump-scale-x: 1; --fa-bounce-jump-scale-y: 1; --fa-bounce-land-scale-x: 1; --fa-bounce-land-scale-y: 1; " ></i></a></td>
+          <td><img class="pozica" src="https://shorturl.at/iwz89" alt="Artenara"></td>
+        </tr>
+        <tr>
+          <td>
+            <form action="/action_page.php">
+              <input type="checkbox" id="Arucas" name="Arucas" value="Arucas">
+            </form>
+          </td>
+          <td>4</td>
+          <td>Arucas es una ciudad y municipio español perteneciente a la isla de Gran Canaria</td>
+          <td>Arucas</td>
+          <td><a href="https://www.arucas.org/modules.php?mod=portal&file=index">Página Oficial de Agaete</a></td>
+          <td><a href="https://shorturl.at/xHL26"><i class="fa-duotone fa-map-location-dot fa-bounce" style=" --fa-bounce-start-scale-x: 1; --fa-bounce-start-scale-y: 1; --fa-bounce-jump-scale-x: 1; --fa-bounce-jump-scale-y: 1; --fa-bounce-land-scale-x: 1; --fa-bounce-land-scale-y: 1; " ></i></a></td>
+          <td><img class="pozica" src="https://shorturl.at/jtKY5" alt="Artenara"></td>
+        </tr>
+      </tbody>
+      </tbody>
+    </table>
+  </div>
+  </body>
+
+</html>
