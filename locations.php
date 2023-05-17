296b8990853571aaec4c09810103752f64b681b2
ADYADY222 <60630331+ADYADY222@users.noreply.github.com>
2023-05-17 13:01:33 +0100

    Update

--- locations.php
+++ locations.php
@@ -31,7 +31,7 @@ fclose($file);
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/duyplus/fontawesome-pro/css/all.min.css">
 </head>
 
 <body>
